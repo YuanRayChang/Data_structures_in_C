a622e9a4313e19452013f345d066a7b8346d0fb6
Yuan-Ray Chang <emanual0426@gmail.com>
2018-09-12 23:47:12 +0800

    strcat

---
+++ strcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void my_strcat(char *s, char *t);
+
+void main()
+{
+	char src[] = "The first.";
+	char src2[] = "The Second.";
+
+	my_strcat(src, src2);
+	printf("After strcat: %s\n", src);
+
+	system("pause");
+}
+
+void my_strcat(char *s, char *t)
+{
+	while(*s)
+		s++;
+	while((*s++ = *t++));
+}
